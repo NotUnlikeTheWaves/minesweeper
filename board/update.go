d3f906d75b3c372e3b1a1818913f61888c917481
NotUnlikeTheWaves <hleistra@gmail.com>
2022-05-17 19:32:57 +0200

    git status

--- board/update.go
+++ board/update.go
@@ -4,14 +4,8 @@ import tea "github.com/charmbracelet/bubbletea"
 
 func (m Board) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
-	// Is it a key press?
 	case tea.KeyMsg:
-
-		// Cool, what was the actual key pressed?
 		switch msg.String() {
-
-		// These keys should exit the program.
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		case "up", "w":
